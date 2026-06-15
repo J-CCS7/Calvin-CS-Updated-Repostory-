4f76c1edf2b5e78af94ce7ed440a99aca178e2ce
Calvin CS <7374calvincarolsen@gmail.com>
2026-06-15 17:41:09 +0700

    Add files via upload

---
+++ Web-WAD-2526-calvincarolsen.com/db.js
@@ -0,0 +1,54 @@
+const menus = [{
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variants: [{
+        price: 2,
+        description: 'Americano',
+        stock: 10,
+    }, {
+        price: 3,
+        description: 'Latte',
+        stock: 7,
+    }],
+    photoUrl: "assets/coffee.jpg"
+}, {
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 10,
+        description: 'Glass',
+        stock: 20,
+    }, {
+        price: 19,
+        description: 'Bottle',
+        stock: 30,
+    }],
+    photoUrl: "assets/wine.jpg"
+}, {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variants: [{
+        price: 8,
+        description: "Slice",
+        stock: 5,
+    }],
+    photoUrl: "assets/pizza.jpg",
+}, {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 3,
+        description: 'Original',
+        stock: 15,
+    }, {
+        price: 6,
+        description: '+Tuna',
+        stock: 10,
+    }, {
+        price: 11,
+        description: '+Grilled Salmon',
+        stock: 5,
+    }],
+    photoUrl: "assets/salad.jpg"
+}
+];
